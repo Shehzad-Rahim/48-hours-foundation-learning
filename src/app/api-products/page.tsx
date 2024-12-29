f57eb47afb46bddb374f51959349621445751bba
Shehzad-Rahim <shehzadrahim33@gmail.com>
2024-12-29 22:19:27 +0500

    local api relative link added after deployment

--- src/app/api-products/page.tsx
+++ src/app/api-products/page.tsx
@@ -1,41 +1,31 @@
 
-// interface Products {
-//     productName : string
-//     productID : number
-//     productColor : string
-//     productPrice : number
-// }
-// async function fetchProducts() : Promise<Products[]> {
-//     const response = await fetch("http://localhost:3000/data.json");
-//     const data = await response.json();
-//     return data.products;
-// }
+interface Products {
+    productName : string
+    productID : number
+    productColor : string
+    productPrice : number
+}
+async function fetchProducts() : Promise<Products[]> {
+    const response = await fetch("https://48-hours-foundation-learning.vercel.app/data.json");
+    const data = await response.json();
+    return data.products;
+}
 
-// async function page() {
-//     const products = await fetchProducts()
+async function page() {
+    const products = await fetchProducts()
     
-//   return (
-//     <div className="flex gap-20 justify-center my-5">
-//         {products.map((item)=> {
-//             return (
-//                 <div key={item.productID}>
-//                     <p>{item.productName}</p>
-//                     <p>{item.productColor}</p>
-//                     <p>{item.productPrice}</p>
-//                 </div>
-//             )
-//         })}
-//     </div>
-//   )
-// }
-
-// export default page
-
-import React from 'react'
-
-function page() {
   return (
-    <div>api product page</div>
+    <div className="flex gap-20 justify-center my-5">
+        {products.map((item)=> {
+            return (
+                <div key={item.productID}>
+                    <p>{item.productName}</p>
+                    <p>{item.productColor}</p>
+                    <p>{item.productPrice}</p>
+                </div>
+            )
+        })}
+    </div>
   )
 }
 
